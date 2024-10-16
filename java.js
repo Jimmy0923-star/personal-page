839304015c54d751c5a5338b05063661a58e49ca
Jimmy0923-star <930923jimmy@gmail.com>
2024-10-16 23:39:19 +0800

    Update java.js

--- java.js
+++ java.js
@@ -1,11 +1,1 @@
-window.addEventListener('scroll', function() {
-    var sections = document.querySelectorAll('section');
-    sections.forEach(function(section) {
-        var position = section.getBoundingClientRect();
-        if (position.top < window.innerHeight && position.bottom >= 0) {
-            section.classList.add('in-view');
-        } else {
-            section.classList.remove('in-view');
-        }
-    });
-});
+
