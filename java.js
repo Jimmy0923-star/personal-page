fd523658cb47c112f49cc00a8e626e484e83d8cc
Jimmy0923-star <930923jimmy@gmail.com>
2024-11-03 13:25:20 +0800

    Update java.js

--- java.js
+++ java.js
@@ -1,3 +1,25 @@
 function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
+
+$('nav a').on('click', function(event) {
+    event.preventDefault();
+    var target = $(this).attr('href');
+    $('html, body').animate({
+        scrollTop: $(target).offset().top
+    }, 1000);
+});
+
+
+$(document).ready(function() {
+    $('#submitBtn').on('click', function() {
+        // 檢查是否有選擇角色
+        if (!$('input[name="character"]:checked').val()) {
+            alert('請選擇一個角色！');
+            return;
+        }
+        
+        // 顯示成功訊息
+        $('#responseMessage').text('謝謝您的投票！');
+    });
+});
